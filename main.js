534d12791380a31e609f93774d1f8b314f90f1d7
felix <felixmetge25@gmail.com>
2024-12-17 16:51:53 +0100

    theme sombre activé !

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
     } else {
       x.style.display = "block";
+    }
+  }
+  var button = document.querySelector(".custom-checkbox input")
+
+button.onclick = function () {
+    let body = document.querySelector('body');
+
+    if (body.classList.contains('dark')) {
+        body.classList.remove('dark');
+    } else {
+        body.classList.add('dark');
     }
   }
